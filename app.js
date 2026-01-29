7397633a4d80f9d3dfe5c136f9598cb2ce8ab1b1
TutuDucktale <tutuchando@ducktaleit.com>
2026-01-29 10:53:00 +0530

    second commit

--- app.js
+++ app.js
@@ -1,74 +1,116 @@
-const todoForm = document.querySelector('form');
-const todoInput = document.getElementById('todo-input');
-const todoListUL = document.getElementById('todo-list');
+const todoForm = document.querySelector("form");
+const todoInput = document.getElementById("todo-input");
+const todoListUL = document.getElementById("todo-list");
+
 
 let allTodos = getTodos();
 updateTodoList();
 
-todoForm.addEventListener('submit', function(e){
+
+todoForm.addEventListener("submit", function (e) {
     e.preventDefault();
     addTodo();
-})
+});
 
-function addTodo(){
+function addTodo() {
     const todoText = todoInput.value.trim();
-    if(todoText.length > 0){
+
+    if (todoText.length > 0) {
         const todoObject = {
             text: todoText,
-            completed: false
-        }
+            completed: false,
+            favorite: false
+        };
+
         allTodos.push(todoObject);
+        saveTodos();
         updateTodoList();
-        saveTodos();
         todoInput.value = "";
-    }  
+    }
 }
-function updateTodoList(){
+
+
+function updateTodoList() {
     todoListUL.innerHTML = "";
-    allTodos.forEach((todo, todoIndex)=>{
-        todoItem = createTodoItem(todo, todoIndex);
+
+    allTodos.forEach((todo, index) => {
+        const todoItem = createTodoItem(todo, index);
         todoListUL.append(todoItem);
-    })
+    });
 }
-function createTodoItem(todo, todoIndex){
-    const todoId = "todo-"+todoIndex;
+
+
+function createTodoItem(todo, todoIndex) {
+    const todoId = "todo-" + todoIndex;
     const todoLI = document.createElement("li");
-    const todoText = todo.text;
     todoLI.className = "todo";
+
     todoLI.innerHTML = `
-        <input type="checkbox" id="${todoId}">
+        <input type="checkbox" id="${todoId}" ${todo.completed ? "checked" : ""}>
+
         <label class="custom-checkbox" for="${todoId}">
-            <svg fill="transparent" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
+            <i class="fa-solid fa-check"></i>
         </label>
+
         <label for="${todoId}" class="todo-text">
-            ${todoText}
+            ${todo.text}
         </label>
-        <button class="delete-button">
-            <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+
+        <button class="edit-button" title="Edit">
+            <i class="fa-solid fa-pen"></i>
         </button>
-    `
-    const deleteButton = todoLI.querySelector(".delete-button");
-    deleteButton.addEventListener("click", ()=>{
-        deleteTodoItem(todoIndex);
-    })
+
+        <button class="favorite-button ${todo.favorite ? "active" : ""}" title="Favorite">
+            <i class="${todo.favorite ? "fa-solid" : "fa-regular"} fa-star"></i>
+        </button>
+
+        <button class="delete-button" title="Delete">
+            <i class="fa-solid fa-trash"></i>
+        </button>
+    `;
+
+    
     const checkbox = todoLI.querySelector("input");
-    checkbox.addEventListener("change", ()=>{
-        allTodos[todoIndex].completed = checkbox.checked;
+    checkbox.addEventListener("change", () => {
+        todo.completed = checkbox.checked;
         saveTodos();
-    })
-    checkbox.checked = todo.completed;
+    });
+
+    
+    todoLI.querySelector(".delete-button").addEventListener("click", () => {
+        allTodos.splice(todoIndex, 1);
+        saveTodos();
+        updateTodoList();
+    });
+
+    
+    todoLI.querySelector(".edit-button").addEventListener("click", () => {
+        const newText = prompt("Edit todo:", todo.text);
+        if (newText && newText.trim()) {
+            todo.text = newText.trim();
+            saveTodos();
+            updateTodoList();
+        }
+    });
+
+    
+    todoLI.querySelector(".favorite-button").addEventListener("click", () => {
+        todo.favorite = !todo.favorite;
+        saveTodos();
+        updateTodoList();
+    });
+
     return todoLI;
 }
-function deleteTodoItem(todoIndex){
-    allTodos = allTodos.filter((_, i)=> i !== todoIndex);
-    saveTodos();
-    updateTodoList();
+
+
+
+
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(allTodos));
 }
-function saveTodos(){
-    const todosJson = JSON.stringify(allTodos);
-    localStorage.setItem("todos", todosJson);
-}
-function getTodos(){
-    const todos = localStorage.getItem("todos") || "[]";
-    return JSON.parse(todos);
+
+
+function getTodos() {
+    return JSON.parse(localStorage.getItem("todos")) || [];
 }
